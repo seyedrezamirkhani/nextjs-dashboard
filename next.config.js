9807f6e4ffea99fd32ad930ef6f77691cb6f217c
Reza Mirkhani <seyedrezamirkhani@yahoo.co.uk>
2024-12-09 16:59:28 +0000

    removed file since the lint setting is now reduced in .eslintrc.json

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// https://nextjs.org/docs/pages/api-reference/next-config-js/eslint
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
